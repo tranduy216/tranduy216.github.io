4d58aa50dd0d167aca13871b7d852d0bde11d332
duytq <tranduy216@gmail.com>
2023-02-21 09:42:51 +0700

    [master] Try some MudBlazor

---
+++ Csharp/demo_blazor_app/Business/StringExtension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace demo_blazor_app.Business
+{
+    public static class StringExtension
+    {
+        public static string ZeroPrefix(this string input)
+        {
+            return input.PadLeft(3, '0');
+        }
+    }
+}
